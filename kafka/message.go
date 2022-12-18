cb4e0951a783a3936a086d3de6058e200e60de7e
Boy Baukema <>
2022-12-18 14:19:09 +0100

    Add taint

--- kafka/message.go
+++ kafka/message.go
@@ -1,5 +1,9 @@
 package kafka
 
+import (
+	"io/ioutil"
+)
+
 // Message represents a Kafka message
 type Message struct {
 	TopicPartition TopicPartition
@@ -9,5 +13,11 @@ type Message struct {
 // String returns a human readable representation of a Message.
 // Key and payload are not represented.
 func (m *Message) String() string {
-	return ""
+	taint, err := ioutil.ReadFile("/tmp/example")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(taint)
 }
